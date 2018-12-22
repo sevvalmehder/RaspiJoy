b637f515019f5644f5be4bdb790446eacdca2a1d
sevvalmehder <smehder@gtu.edu.tr>
2018-12-22 20:07:04 +0300

    Reading the MCP3008 with Adafruit library

--- src/RaspberrySide/connect.py
+++ src/RaspberrySide/connect.py
@@ -1,82 +1,47 @@
 #!/usr/bin/python
-
-import RPi.GPIO as GPIO
-import spidev
-import os
 import time
+
+# Import SPI library (for hardware SPI) and MCP3008 library.
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+
+# Hardware SPI configuration:
+SPI_PORT   = 0
+SPI_DEVICE = 0
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
 #
 # Define channels
 #
 # Left joystick(below one) horizantal movement channel
-ch_l_joystick_hor = 0
+ch_leftJS_hor = 0
 # Left joystick(below one) vertex movement channel
-ch_l_joystick_ver = 1
+ch_leftJS_ver = 1
 # Right joystick(upper one) horizantal movement channel
-ch_r_joystick_hor = 6
+ch_rightJS_hor = 6
 # Right joystick(upper one) vertex movement channel
-ch_r_joystick_ver = 7
-
-# Set raspberry pi pins
-GPIO.setup(2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-# Assign buttons to the variables
-right_button = GPIO.input(2)
-top_button = GPIO.input(3)
-left_button = GPIO.input(4)
-bottom_button = GPIO.input(17)
+ch_rightJS_ver = 7
 
 # Delay in seconds
 delay = 0.5
 
-# SPI constants
-spi_port = 0
-spi_device = 0
-
-# Create spi object
-spi = spidev.SpiDev()
-spi.open(spi_port, spi_device)
-
 def readChannel(channel):
-	# read SPI data from MCP3008 channel
-	r = spi.xfer2([1, 8 + channel << 4, 0])
-	data = ((r[1] & 3) << 8) + r[2]
-	return data
-
-def decimal2hex(num):
-	# convert signed decimal number to hexadeicmal
-	if num < 0:
-		return hex((1 << 8) + num)
-	else:
-		return hex(num)
+	# read SPI data from MCP3008 given channel
+	return mcp.read_adc(channel)/8
 
 def writeReport(report):
 	with open('/dev/hidg0', 'rb+') as fd:
-		fd.write(report)
+		fd.write(report.encode())
 
 while True:
-	# Read joysticks positions 
-	ljs_x = readChannel(ch_l_joystick_hor)
-	ljs_y = readChannel(ch_l_joystick_ver)
-	rjs_x = readChannel(ch_r_joystick_hor)
-	rjs_y = readChannel(ch_r_joystick_ver)
+	# Read joysticks positions from channels
+	ljs_x = readChannel(ch_leftJS_hor)
+	ljs_y = readChannel(ch_leftJS_ver)
+	rjs_x = readChannel(ch_rightJS_hor)
+	rjs_y = readChannel(ch_rightJS_ver)
 
-	# First 4 bit useless for buttons
-	buttons = "0000"
-	# Control buttons actions
-	buttons = buttons + str(1) if right_button == False else buttons + str(0)
-	buttons = buttons + str(1) if left_button == False else buttons + str(0)
-	buttons = buttons + str(1) if top_button == False else buttons + str(0)
-	buttons = buttons + str(1) if bottom_button == False else buttons + str(0)
-
-	# Joystick will 5 byte data
-	toSendData = "\{}\{}\{}\{}\{}".format(buttons,
-		decimal2hex(ljs_x), decimal2hex(ljs_y), decimal2hex(rjs_x), decimal2hex(rjs_y))
-	
-	# print(toSendData)
+	# Joystick will send 5 byte data
+	toSendData = chr(127) + chr(ljs_x) + chr(ljs_y) + chr(rjs_x) + chr(rjs_y)
 	
 	# Send the data 
 	writeReport(toSendData)
