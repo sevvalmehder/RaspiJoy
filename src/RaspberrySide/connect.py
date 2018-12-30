8c036b11dca4cc2baf7c769ab2567267cd72d5f2
sevvalmehder <smehder@gtu.edu.tr>
2018-12-30 14:09:43 +0300

    Buttons informations were added

--- src/RaspberrySide/connect.py
+++ src/RaspberrySide/connect.py
@@ -4,11 +4,21 @@ import time
 # Import SPI library (for hardware SPI) and MCP3008 library.
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
+# Import GPIO
+import RPi.GPIO as GPIO
 
 # Hardware SPI configuration:
 SPI_PORT   = 0
 SPI_DEVICE = 0
 mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+
+# GPIO setup for buttons
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 #
 # Define channels
@@ -22,8 +32,18 @@ ch_rightJS_hor = 6
 # Right joystick(upper one) vertex movement channel
 ch_rightJS_ver = 7
 
+#
+# Define buttons value
+# This values effect the data to be sent 
+# For example when just button3 pressed, joystick will send 4
+# When both button3 and button4 pressed, js will send 12
+val_but1 = 1
+val_but2 = 2
+val_but3 = 4
+val_but4 = 8
+
 # Delay in seconds
-delay = 0.5
+delay = 0.1
 
 def readChannel(channel):
 	# read SPI data from MCP3008 given channel and divide by 2^3 because 
@@ -41,11 +61,22 @@ while True:
 	rjs_x = readChannel(ch_rightJS_hor)
 	rjs_y = readChannel(ch_rightJS_ver)
 
+	buttonInfo = 0;
+	# Control the buttons
+	if not GPIO.input(23):
+		buttonInfo += val_but1
+	if not GPIO.input(24): 
+		buttonInfo += val_but2
+	if not GPIO.input(25):
+		buttonInfo += val_but3
+	if not GPIO.input(12):
+		buttonInfo += val_but4
+
 	# Joystick will send 5 byte data
-	toSendData = chr(127) + chr(ljs_x) + chr(ljs_y) + chr(rjs_x) + chr(rjs_y)
+	toSendData = chr(buttonInfo) + chr(ljs_x) + chr(ljs_y) + chr(rjs_x) + chr(rjs_y)
 	
 	# Send the data 
 	writeReport(toSendData)
 
-	# wait 
+	# wait
 	time.sleep(delay)
